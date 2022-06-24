86fef99cdb13824950af17e456a0b2edce414adb
Nikita Volchok <volue.nik@gmail.com>
2022-06-24 12:39:51 +0300

    Extracted from shadow-dom.

---
+++ src/type-check.js
@@ -0,0 +1,11 @@
+export const tofAppend = node =>
+	tofNode(node) || tofShadowRoot(node);
+
+export const tofShadowRoot = node =>
+	node.constructor && node.constructor.name === 'ShadowRoot';
+
+export const tofNode = node =>
+	node instanceof Node;
+
+export const tofStr = str =>
+	typeof str === 'string';
